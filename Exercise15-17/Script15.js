fcd9930193c1fa7950c1b4eb8af2ee1f16526e27
Oliver Kean L. Palgue <98378847+oliverkean@users.noreply.github.com>
2022-05-13 15:18:20 +0800

    Add files via upload

---
+++ Exercise15-17/Script15.js
@@ -0,0 +1,29 @@
+// firstname
+var firstName = document.getElementById('firstname').value = 'Kean Olibs'
+
+//age
+var age = document.getElementById('age').value = 18
+
+// phone number
+var phoneNum = document.getElementById('phone_num').value = 09276579949
+
+// email
+var email = document.getElementById('email_address').value = '20215296@gmail.com'
+
+// mail address
+var address = document.getElementById('address').value = 'Irisan, Baguio City'
+
+// program
+var program = document.getElementById('program').value = 'Bachelor of Science in Computer Science'
+
+// id number
+var idNum = document.getElementById('id_num').value = 20215296
+
+// year Level
+var yearLevel = document.getElementById('year_level').value = "1st Year"
+
+// section
+var section = document.getElementById('section').value = "IAA1"
+
+alert("Student Information \nFirst Name: " + firstName + " \nAge: " + age + "\n\nContact Information \nPhone Number: " + phoneNum + "\nEmail: " + email 
++ "\nMailing Address: " + address + "\n\nAcademic Information \nProgram: " + program + "\nID Number: "+idNum +"\nYear Level: " + yearLevel + "\nSection: " + section)
